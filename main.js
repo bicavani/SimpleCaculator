5476c6ff3dc1bf17814bb7f62e65c75072b83642
Namlongcomputer <hoandm91@gmail.com>
2019-03-28 17:32:12 +0700

    v1

---
+++ main.js
@@ -0,0 +1,9 @@
+function number(value) {
+    document.form.showNumber.value += value;
+}
+function cle(value) {
+    document.form.showNumber.value = value;
+}
+function evalua() {
+    document.form.showNumber.value = eval(document.form.showNumber.value);
+}
